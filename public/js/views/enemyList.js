a988005b98bbd6c9ccacd796e7fd5f914d6e3dab
Narciso Guillen <guillen.narciso@gmail.com>
2016-04-13 16:42:24 -0500

    Migrate indexJs

--- public/js/views/enemyList.js
+++ public/js/views/enemyList.js
@@ -1,12 +1,23 @@
 Prince.Views.EnemyList = Backbone.View.extend({
 
+  events : {
+    'click .js-active' : 'activate'
+  },
+
   initialize: function(){
     this.$list    = this.$el.find('.list');
     this.template = _.template($('#list-item-template').html());
 
-    this.collection.on('add'   , this.render, this);
-    this.collection.on('remove', this.remove, this);
+    this.collection.on('add'    , this.render, this);
+    this.collection.on('remove' , this.remove, this);
     this.collection.fetch();
+  },
+
+  activate : function(event){
+    var $el   = $(event.currentTarget);
+    var id    = $el.attr('data-id');
+    var model = this.collection.findWhere({cid : id});
+    model.set('active', $el.is(':checked'));
   },
 
   render : function(enemy){
@@ -15,7 +26,8 @@ Prince.Views.EnemyList = Backbone.View.extend({
   },
 
   remove : function(enemy){
-    this.$el.find('[data-id=' + enemy.cid + ']').remove();
+    this.$el.find('tr[data-id=' + enemy.cid + ']').remove();
   }
+
 
 });
--- public/js/views/homeContainer.js
+++ public/js/views/homeContainer.js
@@ -1,5 +1,11 @@
 Prince.Views.HomeContainer = Backbone.View.extend({
+
+  events : {
+    'click .js-attack' : 'attack'
+  },
+
   initialize : function(){
+    this.$attack = this.$el.find('.js-attack');
 
     this.addEnemy = new Prince.Views.AddEnemy({
       el         : '.js-add-enemy',
@@ -11,5 +17,22 @@ Prince.Views.HomeContainer = Backbone.View.extend({
       collection : this.collection
     });
 
+    this.collection.on('change' , this.enableAttack,  this);
+  },
+
+  enableAttack : function(){
+    var active = this.collection.where({active : true});
+    this.$attack.prop('disabled', !active.length);
+  },
+
+  attack : function(){
+    var enemies = _.map(this.collection.where({active : true}), function(enemy){
+      return enemy.toJSON();
+    });
+
+    localStorage.setItem('enemies', JSON.stringify(enemies));
+    window.location.assign('attack');
   }
+
 });
+
