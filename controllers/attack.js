b765218b7d8244364995b2ca78dc3c3f150e61c7
Narciso Guillen <guillen.narciso@gmail.com>
2016-04-13 20:11:38 -0500

    Parse incommng data

--- controllers/attack.js
+++ controllers/attack.js
@@ -4,36 +4,53 @@ const Logger   = require('../lib/logger');
 const path     = require('path');
 const spawn    = require('child_process').spawn;
 const _        = require('underscore');
+const fs       = require('fs');
 
 //TODO Log, refactor
 module.exports = {
   index : function(req, res){ return res.render('attack.html'); },
 
   attack: function(req, res){
-    var host = req.body.host;//XXX
-    var name = req.body.name;//XXX
+    var host     = req.body.host;
+    var name     = req.body.name;
+    var rate     = req.body.rate;
+    var time     = req.body.time;
+    var document = '';
+
+    if(!host || !name || !time || !rate){ return res.status(400).send('Missing Information'); }
+
     var root = path.join(process.cwd(), '/enemies/', name);
 
     _.each(req.body.content, function(data){
-      var headers = JSON.parse(data.headers); //XXX try catch
-      var reqBody = '@' + path.join(root, data.endpoint) + '.json'; // XXX trye catch
+      if(!data.method || !data.endpoint){ return true; }
+      var reqBody = '@' + path.join(root, data.endpoint) + '.json';
+      var rawHeaders;
+      var rawBody;
 
-      console.log(data.method + ' ' + host + data.endpoint);
-      console.log(headers); // Iterate each with \n
-      console.log(reqBody);
+      document+=data.method + ' ' + host + data.endpoint + '\n';
+
+      try{
+        rawHeaders = JSON.parse(data.headers);
+      }catch(error){ rawHeaders = []; }
+
+      try{
+        rawBody = JSON.parse(data.body);
+      }catch(error){ rawBody = null; }
+
+      _.each(rawHeaders, function(header){
+        document+=header + '\n';
+      });
+
+      if(rawBody && !_.isEmpty(rawBody)){ document+=reqBody +'\n\n'; }
     });
 
-    return res.status(200).send('Working on it');
-    //var time     = req.body.time;
-    //var rate     = req.body.rate;
-    //var name     = req.body.name;
-    //if(!time || !rate || !name){ return res.status(400).send('Missing Information'); }
+    fs.writeFileSync(root + '/target', document);
 
-    //name = name.replace(/ /g, '_');
-    //var host   = path.join(process.cwd(), '/enemies/', name, '/host');
-    //var attack = spawn('sh', ['-c', "vegeta attack -targets="+host+" -rate="+rate+" -duration="+time+"s | vegeta report -reporter=plot -output=public/reports/"+name+".html"], { stdio: 'inherit' });
-    //attack.on('close', function(){
-      //return res.send('done');
-    //});
+    var attack = spawn('sh', ['-c', "vegeta attack -targets="+root+"/target -rate="+rate+" -duration="+time+"s | vegeta report -reporter=plot -output=public/reports/"+name+".html"], { stdio: 'inherit' });
+    attack.on('close', function(){
+      return res.send('done'); //SocketIO
+    });
+
+    //return res.status(200).send('Working on it'); //TODO
   }
 };
